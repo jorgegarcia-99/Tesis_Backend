19190cd08f7f008ddd771f82f1e78735644e1a74
Jorge García <jorgealonso.garciacalderon@gmail.com>
2021-06-06 15:41:41 -0500

    Actualizacion DB

--- webapp/config.py
+++ webapp/config.py
@@ -3,7 +3,7 @@ import os
 class Config(object):
     DEBUG = True
     SECRET_KEY = 'dev'
-    DB_URI = 'mongodb://tesis-cosmos:LcCqbpwhQJAuswZIBlbCqVlGrxr5mpx2n77X8wuzpH24sUYNksMHLagP7gsLno5AosZxJFDJNZ8RSXs2nFjYCQ==@tesis-cosmos.mongo.cosmos.azure.com:10255/?ssl=true&retrywrites=false&replicaSet=globaldb&maxIdleTimeMS=120000&appName=@tesis-cosmos@'
+    DB_URI = os.environ['DATABASE_SERVER']
 
 
 class DevelopmentConfig(Config):
